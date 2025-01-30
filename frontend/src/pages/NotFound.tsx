564868c90d6e42be1cdb16d01d9abec3bc53f7ea
Muhammad Hassan <mhassan786000@gmail.com>
2025-01-30 23:56:52 +0400

    add not found page

---
+++ frontend/src/pages/NotFound.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Typography, Box } from "@mui/material";
+
+const NotFoundPage: React.FC = () => {
+  return (
+    <Box>
+      <Typography variant="h4">Page Not Found</Typography>
+    </Box>
+  );
+};
+
+export default NotFoundPage;
